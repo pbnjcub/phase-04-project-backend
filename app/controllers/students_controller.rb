57e11f838b87ccfdda33b4ed0d431d8aec1db2c2
Paul Jung <pbnjcub@gmail.com>
2023-03-30 15:09:55 -0400

    added index and show actions

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,15 @@
 class StudentsController < ApplicationController
     def index
-        @students = Student.all
-        render json: @students
+        students = Student.all
+        render json: students
     end
 
     def show
-        @student = Student.find(params[:id])
-        render json: @student
+        student = Student.find_by(id: params[:id])
+        if student
+            render json: student
+        else
+            render json: {error: "Student not found"}, status: 404
+        end
     end
 end
