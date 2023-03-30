faf3574c922fcc7f776dadf7d41058423a411c5e
Paul Jung <pbnjcub@gmail.com>
2023-03-29 20:03:40 -0400

    add initial data

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Phase04ProjectBackend
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins "*"
