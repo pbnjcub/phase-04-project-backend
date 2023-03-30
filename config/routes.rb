647341ed934c24053c5a0d98d59868df242dd8cd
Paul Jung <pbnjcub@gmail.com>
2023-03-30 11:08:42 -0400

    connected to front end successfully

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "/students/:id", to: "students#show"
+  get "/students", to: "students#index"
 end
