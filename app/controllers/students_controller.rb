25e1bffa79eec7fdfc3a991a3f361741f5a72766
Paul Jung <pbnjcub@gmail.com>
2023-03-31 09:33:25 -0400

    added new student form for teacher view

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,4 +12,19 @@ class StudentsController < ApplicationController
             render json: {error: "Student not found"}, status: 404
         end
     end
+
+    def create
+        student = Student.new(student_params)
+        if student.save
+            render json: student, status: :created
+        else
+            render json: student.errors, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def student_params
+        params.require(:student).permit(:first_name, :last_name)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get "/students/:id", to: "students#show"
-  get "/students", to: "students#index"
+  resources :students, only: [:index, :show, :create]
 end
