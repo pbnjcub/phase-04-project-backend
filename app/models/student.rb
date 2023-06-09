129b08f9c3d00ad01f9bb475ba5a76412f14dc66
Paul Jung <pbnjcub@gmail.com>
2023-06-09 14:58:23 -0400

    new students and courses update correctly

--- app/models/student.rb
+++ app/models/student.rb
@@ -4,4 +4,5 @@ class Student < ApplicationRecord
 
     validates :first_name, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
     validates :last_name, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
+
 end
