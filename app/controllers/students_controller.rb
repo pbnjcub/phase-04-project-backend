91d2daa6cebd44c5f4bf323d39d8bc9b70aae7ae
Paul Jung <pbnjcub@gmail.com>
2023-06-12 17:10:51 -0400

    fixing bugs

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -50,7 +50,7 @@ class StudentsController < ApplicationController
     private
 
     def student_params
-        params.require(:student).permit(:first_name, :last_name)
+        params.require(:student).permit(:id, :first_name, :last_name)
     end
 
 end
